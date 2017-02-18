1780fc0af7fa791d8c3e9e5d955219a675f64e59
Nitid Rakfaeng <nitid.aom@mail.kmutt.ac.th>
2017-02-18 16:40:50 +0700

    (test git) add attribute class Account

--- src/lostitemproject/Account.java
+++ src/lostitemproject/Account.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lostitemproject;
 
-/**
- *
- * @author POOM
- */
+
 public class Account {
-    
+    private int accId;
 }
