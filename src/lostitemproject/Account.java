fce903249475913870ddaf2b57e619640f356c1e
Kunanya1 <kunan.taw1998@gmail.com>
2017-03-16 17:55:53 +0700

    edit class picture

--- src/lostitemproject/Account.java
+++ src/lostitemproject/Account.java
@@ -1,6 +1,7 @@
 package lostitemproject;
 
 public class Account {
+
     private int accId;
     private String name;
     private String surname;
@@ -8,13 +9,88 @@ public class Account {
     private String phone;
     private String line;
     private String email;
-    
+    private String password;
+
     //login >>> boolean
     //
     //getAccId return 10
-    public Account(){
-        
+    public Account() {
+
+    }
+
+    public Account(String name, String surname, String username,String password, String phone, String line, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.phone = phone;
+        this.line = line;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getAccId() {
+        return accId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
     
     
+    public boolean login(String username,String password) {
+        boolean result=true;
+        if(password.equals(password)){
+            result = true;
+        }    
+        return result;
+    }
+
 }
